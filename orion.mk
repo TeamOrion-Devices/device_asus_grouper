b23aea0c8ae3ab01319dcd666ae5ec13c4a3288f
srisurya95 <srisurya95@gmail.com>
2015-07-05 19:03:03 +0530

    Orion-ify

--- orion.mk
+++ orion.mk
@@ -6,17 +6,17 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 800
 
 # Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/orion/config/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/orion/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/grouper/full_grouper.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grouper
-PRODUCT_NAME := slim_grouper
+PRODUCT_NAME := orion_grouper
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
